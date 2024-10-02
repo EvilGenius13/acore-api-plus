a7f3ae03db4543680a454012cdd8a7fd4ec14302
Josh Faigan <djjoker13@gmail.com>
2024-10-02 16:20:18 -0400

    had to redo docke setup due to nginx issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3005',
-        changeOrigin: true,
-      },
-    },
-  },
+  base: './',
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3005',
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 });
